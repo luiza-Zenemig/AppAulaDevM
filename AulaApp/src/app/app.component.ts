ff88d401fcbd510fe5ac07aef3ceb8a1f431e1ff
Luiza Gimenez <you@example.com>
2022-03-30 13:31:06 -0400

    commit

---
+++ AulaApp/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Navegação', url: 'pages/navegação', icon: 'planet'},
+    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Play', url: '/play', icon: 'play'},
+    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Profile', url: '/profile', icon: 'finger-print'},
+    { title: 'LogOut', url: '/logout', icon: 'log-out'},
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() {}
+}
